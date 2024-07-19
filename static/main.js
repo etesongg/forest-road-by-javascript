5f3f81a3c6e5feaa4c0a210beef3c1601ae392d7
Rosie Seok <lovelyrosie731@gmail.com>
2024-07-19 21:03:36 +0900

    seok

--- static/main.js
+++ static/main.js
@@ -1,96 +1,202 @@
+/* 섹션2) 산 목록 슬라이드로 보여주기 - 시작 */
 document.addEventListener("DOMContentLoaded", function () {
-  const apiUrl =
-    "http://openapi.forest.go.kr/openapi/service/cultureInfoService/gdTrailInfoOpenAPI";
-  const imageApiUrl =
-    "http://openapi.forest.go.kr/openapi/service/cultureInfoService/gdTrailInfoImgOpenAPI";
-  const serviceKey =
-    "6OOYIpPBcuzSlb9ySCOig5hl2yyAicP6cs%2BG7wM3kb%2B1AdFH9fn5nAymyaCjxrnF5YQnIGxoSnIZlT9cvhjrRg%3D%3D";
-  const itemsPerPage = 6; // 페이지당 항목 수
-  let currentPage = 1; // 현재 페이지 번호
-  const noImageUrl = "https://via.placeholder.com/200?text=No+Image"; // "No Image" 이미지 URL
-  // 산 데이터를 가져오는 함수
-  function fetchMountains(page) {
-    // API 호출 URL 생성
-    const url = `${apiUrl}?serviceKey=${serviceKey}&pageNo=${page}&numOfRows=${itemsPerPage}&_type=json`;
-    // API 호출
-    fetch(url)
-      .then((response) => response.json()) // JSON 형태로 응답을 파싱
-      .then((data) => {
-        console.log("Fetched data:", data); // 가져온 데이터 콘솔에 출력
-        displayMountains(data.response.body.items.item); // 산 데이터를 화면에 표시
-        setupPagination(data.response.body.totalCount, page); // 페이지네이션 설정
-      })
-      .catch((error) => console.error("Error fetching data:", error)); // 에러 처리
-  }
-  // 산 데이터를 화면에 표시하는 함수
-  function displayMountains(mountains) {
-    const mountainList = document.getElementById(
-      "main-mountain-list-container"
-    ); // 산 목록을 표시할 HTML 요소
-    mountainList.innerHTML = ""; // 기존 내용을 초기화
-    mountains.forEach((mountain) => {
-      // 각 산에 대해 이미지를 가져오는 함수 호출
-      fetchImage(mountain.mntncd)
-        .then((imageUrl) => {
-          const mountainItem = document.createElement("div");
-          mountainItem.className = "mountain-item";
-          mountainItem.innerHTML = `
-                          <div class="main-mountain-list-item" style="background-image: url(${imageUrl})">
-                              <div class="main-mountain-list-overlay">
-                                  <h3>${mountain.subnm}</h3> <!-- 산 부제 -->
-                                  <p>지역: ${mountain.areanm}</p> <!-- 지역명 -->
-                                  <p>높이: ${mountain.mntheight}m</p> <!-- 산 높이 -->
-                              </div>
-                          </div>
-                          <div class="main-mountain-list-item-title">
-                          <h2>${mountain.mntnm}</h2> <!-- 산 이름 -->
-                          </div>
-                      `;
-          mountainList.appendChild(mountainItem); // 산 항목을 목록에 추가
-        })
-        .catch((error) => console.error("Error fetching image:", error));
-    });
-  }
-  // 산 이미지 데이터를 가져오는 함수
-  function fetchImage(mntncd) {
-    return new Promise((resolve, reject) => {
-      const url = `${imageApiUrl}?searchWrd=${mntncd}&ServiceKey=${serviceKey}&_type=json`;
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-          if (
-            data.response.body.items &&
-            data.response.body.items.item.length > 0
-          ) {
-            const image = data.response.body.items.item[0].image; // 첫 번째 이미지를 사용
-            const imageUrl = `http://www.forest.go.kr/swf/foreston/mountain/${image}`;
-            resolve(imageUrl);
-          } else {
-            resolve(noImageUrl);
-          }
-        })
-        .catch((error) => {
-          resolve(noImageUrl);
-          console.error("Error fetching image:", error);
-        });
-    });
-  }
-  // 페이지네이션을 설정하는 함수
-  function setupPagination(totalItems, currentPage) {
-    const pagination = document.getElementById("pagination"); // 페이지네이션을 표시할 HTML 요소
-    pagination.innerHTML = ""; // 기존 페이지네이션 초기화
-    const totalPages = Math.ceil(totalItems / itemsPerPage); // 전체 페이지 수 계산
-    // 각 페이지 버튼을 생성하여 추가
-    for (let i = 1; i <= totalPages; i++) {
-      const button = document.createElement("button");
-      button.className = "pagination-button";
-      button.innerText = i; // 버튼에 페이지 번호 표시
-      if (i === currentPage) {
-        button.style.fontWeight = "bold"; // 현재 페이지 버튼을 굵게 표시
-      }
-      button.addEventListener("click", () => fetchMountains(i)); // 버튼 클릭 시 해당 페이지로 이동
-      pagination.appendChild(button); // 페이지 버튼을 페이지네이션에 추가
-    }
-  }
-  fetchMountains(currentPage); // 초기 페이지 데이터 가져오기
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 6,
+    spaceBetween: 30,
+    centeredSlides: false,
+    loop: true,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+    speed: 3000,
+    effect: "slide",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 });
+/* 섹션2) 산 목록 슬라이드로 보여주기 - 끝 */
+
+/* 섹션3) 추천코스 보여주기 - 시작*/
+function showInfo(place) {
+  let details = {
+    북한산: [
+      {
+        title: "비봉 코스",
+        description: "고소공포증 없는 산쟁이 추천",
+        difficulty: "어려움",
+        time: "1시간 30분",
+        distance: "3.0km",
+      },
+      {
+        title: "백운대 코스",
+        description: "북한산 정상 백운대 최단 코스",
+        difficulty: "보통",
+        time: "1시간 30분",
+        distance: "2.0km",
+      },
+
+      {
+        title: "원효봉 코스",
+        description: "북한산 입문용으로 추천 코스",
+        difficulty: "쉬움",
+        time: "1시간 30분",
+        distance: "2.7km",
+      },
+    ],
+    설악산: [
+      {
+        title: "오색 코스",
+        description: "조망을 잃고 정상을 빨리 만나는 코스",
+        difficulty: "매우 어려움",
+        time: "3시간",
+        distance: "5.0km",
+      },
+      {
+        title: "한계령 코스",
+        description: "멋진 조망을 얻고 다리를 잃는 코스",
+        difficulty: "어려움",
+        time: "3시간 30분",
+        distance: "8.3km",
+      },
+
+      {
+        title: "소공원 코스",
+        description: "계곡을 얻고 다리를 잃는 최장 코스",
+        difficulty: "매우 어려움",
+        time: "5시간",
+        distance: "11.0km",
+      },
+    ],
+    도봉산: [
+      {
+        title: "신선대 코스",
+        description: "도봉산 정상 최단 코스",
+        difficulty: "어려움",
+        time: "2시간",
+        distance: "3.3km",
+      },
+      {
+        title: "오봉 코스",
+        description: "멋진 뷰와 계곡길을 한번에",
+        difficulty: "보통",
+        time: "1시간 30분",
+        distance: "3.5km",
+      },
+
+      {
+        title: "우이암 코스",
+        description: "초보 산쟁이 혼산 추천코스",
+        difficulty: "쉬움",
+        time: "1시간 30분",
+        distance: "3.0km",
+      },
+    ],
+    소백산: [
+      {
+        title: "어의곡 코스",
+        description: "정상으로 가는 초보 추천 코스",
+        difficulty: "보통",
+        time: "2시간 30분",
+        distance: "5.1km",
+      },
+      {
+        title: "천동계곡 코스",
+        description: "계곡을 끼고 정상으로 가는 코스",
+        difficulty: "보통",
+        time: "3시간",
+        distance: "6.8km",
+      },
+
+      {
+        title: "삼가동 코스",
+        description: "주차 찬스를 쓴다면? 정상 최단 코스!",
+        difficulty: "보통",
+        time: "2시간 30분",
+        distance: "5.5km",
+      },
+    ],
+    한라산: [
+      {
+        title: "성판악 코스",
+        description: "길지만 완만한 숲길을 따라 가는 코스",
+        difficulty: "보통",
+        time: "4시간 30분",
+        distance: "9.6km",
+      },
+      {
+        title: "관음사 코스",
+        description: "가파르지만 멋진 풍경을 볼 수 있는 코스",
+        difficulty: "어려움",
+        time: "5시간",
+        distance: "8.7km",
+      },
+
+      {
+        title: "영실 코스",
+        description: "윗세오름으로 가는 경치 좋은 코스",
+        difficulty: "보통",
+        time: "2시간 30분",
+        distance: "5.8km",
+      },
+    ],
+    관악산: [
+      {
+        title: "사다능선 코스",
+        description: "길지만 전망이 가장 좋은 코스",
+        difficulty: "어려움",
+        time: "2시간 30분",
+        distance: "3.8km",
+      },
+      {
+        title: "서울대 신공학관 코스",
+        description: "정상으로 가는 가장 짧고 쉬운 코스",
+        difficulty: "보통",
+        time: "1시간 30분",
+        distance: "1.7km",
+      },
+
+      {
+        title: "과천향교 코스",
+        description: "시원한 계곡을 따라가는 코스",
+        difficulty: "쉬움",
+        time: "1시간 30분",
+        distance: "3.2km",
+      },
+    ],
+  };
+
+  let courses = details[place];
+  let content = "";
+
+  courses.forEach((course) => {
+    content += `
+          <div class="course-box">
+              <div class="course-info">
+                  <div class="course-title">${course.title}</div>
+                  <div class="course-description">${course.description}</div>
+              </div>
+              <div class="course-details">
+                  <div class="detail-item difficulty">
+                      <span class="icon">&#x1F4C8;</span>
+                      <span class="text difficulty-text">${course.difficulty}</span>
+                  </div>
+                  <div class="detail-item time">
+                      <span class="icon">&#x23F1;</span>
+                      <span class="text">${course.time}</span>
+                  </div>
+                  <div class="detail-item distance">
+                      <span class="icon">&#x1F6B6;</span>
+                      <span class="text">${course.distance}</span>
+                  </div>
+              </div>
+          </div>
+      `;
+  });
+
+  document.getElementById("recommendation-details").innerHTML = content;
+}
+
+/* 섹션3) 추천코스 보여주기 - 끝 */
