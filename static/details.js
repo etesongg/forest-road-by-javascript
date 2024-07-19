48f9922481de50a68664a1d8edd6e92689b37eb4
etesong014 <etesong0114@gmail.com>
2024-07-19 22:33:49 +0900

    Feat: 디테일 페이지 정보 추가 - 툴팁, 팝오버 사용 - 네이버 지도, 지오코드

--- static/details.js
+++ static/details.js
@@ -10,29 +10,21 @@ const MOUNTAINS_KEY = config.KEY.mountains_key;
 const MOUNTAINS_URL = config.URL.mountains_url;
 const TRAIL_URL = config.URL.trail_url;
 
-let url = new URL(
-  `${MOUNTAINS_URL}1400000/service/cultureInfoService2/mntInfoOpenAPI2?_type=json&serviceKey=${MOUNTAINS_KEY}`
-);
+const mountain_keyword = "설악산";
 
 // 산 정보만 뽑을 경우
-const getData = async () => {
+const getMntData = async () => {
   url = new URL(
-    `${MOUNTAINS_URL}1400000/service/cultureInfoService2/mntInfoOpenAPI2?_type=json&serviceKey=${MOUNTAINS_KEY}&searchWrd=불암산`
+    `${MOUNTAINS_URL}1400000/service/cultureInfoService2/mntInfoOpenAPI2?_type=json&serviceKey=${MOUNTAINS_KEY}&searchWrd=${mountain_keyword}`
   );
   const response = await fetch(url);
   const data = await response.json();
-  const mntilistNo = data.response.body.items.item[0].mntilistno; // 산 코드
-  console.log(data);
+  console.log("getMntData", data);
+  const mntilistNo = data.response.body.items.item.mntilistno; // 산 코드
   await getMntImgData(mntilistNo);
-  await getMntTrail(mntilistNo);
-  displayMntInfoArticle({
-    mntiName: data.response.body.items.item[0].mntiname,
-    mntiHigh: data.response.body.items.item[0].mntihigh,
-    mntiRegion: data.response.body.items.item[0].mntiadd,
-    mntiDetails: data.response.body.items.item[0].mntidetails,
-  });
+  getTrailData();
 };
-getData();
+getMntData();
 
 const getMntImgData = async (mntilistNo) => {
   url = new URL(
@@ -68,22 +60,39 @@ const displayMntInfoFigure = (MntImgFile) => {
 
 const displayMntInfoArticle = (info) => {
   const mntiDetails =
-    info.mntiDetails.length >= 300
-      ? `${info.mntiDetails.slice(0, 300)}...`
-      : info.mntiDetails;
+    info.details.length >= 300
+      ? `${info.details.slice(0, 300)}...`
+      : info.details;
   document.querySelector(".details-mnt-information article").innerHTML = `
-        <h5><b>${info.mntiName}</b></h5>
-        <p><span>고도 |</span> <span>${info.mntiHigh}m</span></p>
-        <p><span>지역 |</span> <span>${info.mntiRegion}</span></p>
-        <p><span>소개 |</span> <span>${mntiDetails}</span></p>`;
+        <h5><b>${info.mntnm}</b></h5>
+        <p><span>고도 |</span> <span>${info.mntheight} m</span></p>
+        <p><span>지역 |</span> <span>${info.areanm}</span></p>
+        <p><span>소개 |</span> <span>${mntiDetails}</span></p>
+        
+        <b>추가로 100대명산 선정이유, 산정보개관, 대중교통정보설명, 주변관광정보설명, 산정보주변관광정보기타코스설명 보이게 할 예정</b>`;
 };
 
-const getMntTrail = async (mntilistNo) => {
+const getMntTrail = async (mntncd) => {
   url = new URL(
-    `${MOUNTAINS_URL}1400000/service/cultureInfoService2/frtrlSectnOpenAPI2?_type=json&mntiListNo=${mntilistNo}&ServiceKey=${MOUNTAINS_KEY}`
+    `${TRAIL_URL}openapi/service/cultureInfoService/gdTrailInfoImgOpenAPI?_type=json&searchWrd=${mntncd}&serviceKey=${MOUNTAINS_KEY}`
   );
   const response = await fetch(url);
   const data = await response.json();
-
-  console.log("숲길", data);
+  console.log("getMntTrail", data);
+  const trailArray = data.response.body.items.item;
+  displayMntTrailInfo(trailArray);
+};
+// titl	산행추가제목
+// content	산행추가설명
+// image	산행이미지순번
+const displayMntTrailInfo = async (trailArray) => {
+  const MntTrailInfoHTML = trailArray
+    .map((trail) => {
+      return `
+        <img src="http://www.forest.go.kr/swf/foreston/mountain/${trail.image}" alt="">
+        <p>${trail.titl}</p>
+        <p>${trail.content}</p>`;
+    })
+    .join("");
+  document.querySelector(".details-mnt-trail").innerHTML = MntTrailInfoHTML;
 };
