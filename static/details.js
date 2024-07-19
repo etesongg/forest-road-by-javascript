620c2597d3a26a0abbf245b7c478264a1e2df815
etesong014 <etesong0114@gmail.com>
2024-07-19 22:33:49 +0900

    Feat: 디테일 페이지 정보 추가 - 툴팁, 팝오버 사용 - 네이버 지도, 지오코드

--- static/details.js
+++ static/details.js
@@ -3,12 +3,15 @@ import config from "../apikey.js";
 
 const MOUNTAINS_KEY = config.KEY.mountains_key;
 const MOUNTAINS_URL = config.URL.mountains_url;
+const TRAIL_URL = config.URL.trail_url;
+const WEATHER_URL = config.URL.weather_url;
+const WEATHER_KEY = config.KEY.weather_key;
 
 let url = new URL(
   `${MOUNTAINS_URL}1400000/service/cultureInfoService2/mntInfoOpenAPI2?_type=json&serviceKey=${MOUNTAINS_KEY}`
 );
 
-const mountain_keyword = "한라산";
+const mountain_keyword = "설악산";
 
 // 산 정보만 뽑을 경우
 const getMntData = async () => {
@@ -17,20 +20,38 @@ const getMntData = async () => {
   );
   const response = await fetch(url);
   const data = await response.json();
-  console.log("getMntData", data);
-  const mntilistNo = data.response.body.items.item.mntilistno; // 산 코드
-  const mntiadd = Array.isArray(data.response.body.items.item)
-    ? data.response.body.items.item[0].mntiadd
-    : data.response.body.items.item.mntiadd; // 산 주소
+  const mntilistNo = data.response.body.items.item[0].mntilistno; // 산 코드
+  console.log(data);
   await getMntImgData(mntilistNo);
-  await getTrailData();
-  await translateToAddress(mntiadd);
+  await getMntTrail(mntilistNo);
+  displayMntInfoArticle({
+    mntiName: data.response.body.items.item[0].mntiname,
+    mntiHigh: data.response.body.items.item[0].mntihigh,
+    mntiRegion: data.response.body.items.item[0].mntiadd,
+    mntiDetails: data.response.body.items.item[0].mntidetails,
+  });
 };
 getMntData();
 
 const getMntImgData = async (mntilistNo) => {
   url = new URL(
     `${MOUNTAINS_URL}1400000/service/cultureInfoService2/mntInfoImgOpenAPI2?_type=json&mntiListNo=${mntilistNo}&ServiceKey=${MOUNTAINS_KEY}`
+  );
+  const response = await fetch(url);
+  const data = await response.json();
+  const MntImgFile = data.response.body.items.item[0].imgfilename;
+  displayMntInfoFigure(MntImgFile);
+};
+
+const displayMntInfoFigure = (MntImgFile) => {
+  document.querySelector(
+    ".details-mnt-information figure"
+  ).innerHTML = `<img src="http://www.forest.go.kr/images/data/down/mountain/${MntImgFile}" alt="">`;
+};
+
+const getTrailData = async () => {
+  url = new URL(
+    `${TRAIL_URL}openapi/service/cultureInfoService/gdTrailInfoOpenAPI?_type=json&searchMtNm=${mountain_keyword}&serviceKey=${MOUNTAINS_KEY}`
   );
   const response = await fetch(url);
   const data = await response.json();
@@ -72,24 +93,10 @@ const displayMntInfoArticle = (info) => {
       ? `${info.details.slice(0, 300)}...`
       : info.details;
   document.querySelector(".details-mnt-information article").innerHTML = `
-        <h5><b>${info.mntnm}</b></h5>
-        <p><span>고도 |</span> <span>${info.mntheight} m</span></p>
-        <p><span>지역 |</span> <span>${info.areanm}</span></p>
-        <p><span>소개 |</span> <span>${mntiDetails}</span></p>
-        
-        <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
-    <i class="fa-solid fa-circle-info"></i> 추가 정보
-  </a>
-    <div class="collapse" id="collapseExample">
-    <div class="card card-body">
-        <b>추가로 100대명산 선정이유 | ${info.aeatreason}
-        <br>산정보개관 | ${info.overview}
-        <br>대중교통정보설명 | ${info.transport}
-        <br>주변관광정보설명 | ${info.tourisminf}</b>
-    </div>
-    </div>`;
-
-  openTrailCourse(info.etccourse);
+        <h5><b>${info.mntiName}</b></h5>
+        <p><span>고도 |</span> <span>${info.mntiHigh}m</span></p>
+        <p><span>지역 |</span> <span>${info.mntiRegion}</span></p>
+        <p><span>소개 |</span> <span>${mntiDetails}</span></p>`;
 };
 
 const getMntTrail = async (mntncd) => {
@@ -98,85 +105,8 @@ const getMntTrail = async (mntncd) => {
   );
   const response = await fetch(url);
   const data = await response.json();
-  console.log("getMntTrail", data);
-  const trailArray = data.response.body.items.item;
-  displayMntTrailInfo(trailArray);
-};
 
-// 산행 content, HTML 엔티티로 인코딩
-const decodeHTMLEntities = (text) => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(text, "text/html");
-  return doc.documentElement.textContent;
-};
-
-// titl	산행추가제목
-// content	산행추가설명
-// image	산행이미지순번
-// 신행 아니고 그냥 관련 사진? 같은 느낌
-const displayMntTrailInfo = async (trailArray) => {
-  const MntTrailInfoHTML = trailArray
-    .map((trail) => {
-      const splitTitle = trail.titl.includes("-")
-        ? trail.titl.split("-")[1]
-        : trail.titl;
-      const decodedContent = decodeHTMLEntities(trail.content);
-      return `
-        <div class="card" style="width: 18rem;">
-        <img src="http://www.forest.go.kr/swf/foreston/mountain/${trail.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <p class="card-text">
-            <strong>${splitTitle}</strong><br>
-            ${decodedContent}
-            </p>
-        </div>
-        </div>`;
-    })
-    .join("");
-  document.querySelector(".details-mnt-related-images > .row").innerHTML =
-    MntTrailInfoHTML;
-};
-
-const translateToAddress = async (mntAdress) => {
-  naver.maps.Service.geocode(
-    { address: mntAdress },
-    function (status, response) {
-      console.log(mntAdress);
-      if (status === naver.maps.Service.Status.ERROR) {
-        console.log("Something wrong!");
-        console.log("Status:", status);
-        console.log("Response:", response);
-        return;
-      }
-      // 성공 시의 response 처리
-      const { x, y } = response.v2.addresses[0];
-      console.log(`y : ${x} x : ${y}`);
-      return initMap(y, x), callWeather(y, x);
-    }
-  );
-};
-
-// 지도 생성
-var map = null;
-
-function initMap(x, y) {
-  var map = new naver.maps.Map("map", {
-    center: new naver.maps.LatLng(x, y),
-    zoom: 15,
-  });
-
-  var marker = new naver.maps.Marker({
-    position: new naver.maps.LatLng(x, y),
-    map: map,
-  });
-}
-
-const openTrailCourse = (etccourse) => {
-  const trailCourseIcon = document.querySelector(".fa-location-arrow");
-  trailCourseIcon.setAttribute("data-bs-content", etccourse);
-  new bootstrap.Popover(trailCourseIcon, {
-    html: true,
-  }); // 부트스트랩 popover 초기화 해줘야 함, html 태그 인식되도록 하기
+  console.log("숲길", data);
 };
 
 // const callWeather = async (y, x) => {
