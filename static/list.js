a4db8f924c04907b77450237a8aca886e0a424c6
chaeyeon <cysy0424@gmail.com>
2024-07-20 19:15:26 +0900

    list페이지 기능 추가

--- static/list.js
+++ static/list.js
@@ -1,7 +1,13 @@
-const MOUNTAINS_KEY = `AXa61V6%2FXJxE8zJezVhN70U9qWt%2BwsYy%2BjtQkZj7dFMID%2FmdDwFu%2BlWAXpHw6deAA7IVCLauk5FAsBiv98OXhA%3D%3D`;
+import config from "../apikey.js"
+const MOUNTAINS_KEY = config.KEY.mountains_key;
+const MOUNTAINS_URL = config.URL.mountains_url;
+const TRAIL_URL = config.URL.trail_url;
+
+
+// const MOUNTAINS_KEY = `AXa61V6%2FXJxE8zJezVhN70U9qWt%2BwsYy%2BjtQkZj7dFMID%2FmdDwFu%2BlWAXpHw6deAA7IVCLauk5FAsBiv98OXhA%3D%3D`;
 
 let url = new URL(
-  `http://openapi.forest.go.kr/openapi/service/cultureInfoService/gdTrailInfoOpenAPI?searchWrd=2619990400&ServiceKey=${MOUNTAINS_KEY}&numOfRows=18`
+  `${TRAIL_URL}openapi/service/cultureInfoService/gdTrailInfoOpenAPI?searchWrd=2619990400&ServiceKey=${MOUNTAINS_KEY}&numOfRows=18`
 );
 
 const searchButton = document.querySelector("#SearchButton");
@@ -20,32 +26,33 @@ const groupSize = 5;
 const numOfRows = 12;
 
 
+const mountain_keyword = ``;
+
+
 
 
 // 산 리스트를 가져오는 함수
 const getMntiList = async () => {
   const keyword = document.getElementById("SearchName").value;
   url = new URL(
-    `http://openapi.forest.go.kr/openapi/service/cultureInfoService/gdTrailInfoOpenAPI?ServiceKey=${MOUNTAINS_KEY}&numOfRows=${numOfRows}&searchMtNm=${keyword}&_type=json`
+    `${TRAIL_URL}openapi/service/cultureInfoService/gdTrailInfoOpenAPI?ServiceKey=${MOUNTAINS_KEY}&numOfRows=${numOfRows}&searchMtNm=${keyword}&_type=json`
   );
   url.searchParams.set("pageNo", pageNo)
   const response = await fetch(url);
-  // url.searchParams.set("pageNo", pageNo) //왜 안되는지
   const data = await response.json();
+  
   mntiList = data.response.body.items.item;
   nameList = mntiList.map((name) => name.mntnm);
   codeList = mntiList.map((code) => code.mntncd);
   for (let i = 0; i < codeList.length; i++) {
-    await getMntiImg(codeList[i], i); // 인덱스를 인수로 전달
+    await getMntiImg(codeList[i], i);
   }
-  // console.log(codeList) //421100101, 427700101, 478400101, 317100101, 414800101, 457700101, 441500301, 427206801, 427200401, 116200201, 437200401, 488400801
-  render(); // 모든 이미지 로딩 후 렌더링 함수 호출
-  // paginationRender();
+  await render();
 };
 
 const getMntiImg = async (mntncd, index) => {
   url = new URL(
-    `http://apis.data.go.kr/1400000/service/cultureInfoService2/mntInfoImgOpenAPI2?mntiListNo=${mntncd}&numOfRows=${numOfRows}&ServiceKey=${MOUNTAINS_KEY}&_type=json`
+    `${MOUNTAINS_URL}1400000/service/cultureInfoService2/mntInfoImgOpenAPI2?mntiListNo=${mntncd}&numOfRows=${numOfRows}&ServiceKey=${MOUNTAINS_KEY}&_type=json`
   );
 
   const response = await fetch(url);
@@ -65,23 +72,43 @@ const getMntiImg = async (mntncd, index) => {
 
 
 
-
+// 검색 창에 입력 후 산 목록 가져오기
 const getMntiListByKeyword = async () => {
   const keyword = document.getElementById("SearchName").value;
+  if (keyword == ``) {
+    return;
+  }
   url = new URL(
-    `http://openapi.forest.go.kr/openapi/service/cultureInfoService/gdTrailInfoOpenAPI?ServiceKey=${MOUNTAINS_KEY}&numOfRows=${numOfRows}&searchMtNm=${keyword}&_type=json`
+    `${TRAIL_URL}openapi/service/cultureInfoService/gdTrailInfoOpenAPI?ServiceKey=${MOUNTAINS_KEY}&numOfRows=${numOfRows}&searchMtNm=${keyword}&_type=json`
   );
   pageNo = 1;
   url.searchParams.set("pageNo", pageNo)
   const response = await fetch(url);
   const data = await response.json();
+  if(data.response.body.items == '') {
+    errorRender(keyword)
+    return;
+  }
   mntiList = data.response.body.items.item;
   render()
   document.getElementById("SearchName").value = ``;
+  searchButton.addEventListener("click", getMntiListByKeyword)
 };
 
-searchButton.addEventListener("click", getMntiListByKeyword)
 
+
+//검색 창 엔터키로 입력 기능
+document.getElementById("SearchName").addEventListener("keyup", (enterKeyCode) => {
+  let enterKey = enterKeyCode.code;
+  if (enterKey == "Enter" || enterKey == "NumpadEnter") {
+    if(document.getElementById("SearchName").value == "") {
+      return;
+    } else if(document.getElementById("SearchName").value) {
+      getMntiListByKeyword()
+      document.getElementById("SearchName").value = ``;
+    }
+  }
+})
 
 
 
@@ -92,10 +119,11 @@ const render = () => {
     .map((mnti, index) => {
       let imgSrc = imgList[index] || ""; // 각 산의 이미지를 imgList 배열에서 참조
       return `<article class="Mountain">
-      <div class="Image"><a><img src="${imgSrc}" alt="No Image"></a></div>
+      <div class="Image"><img src="${imgSrc}" alt="No Image" onError='https://static.vecteezy.com/system/resources/thumbnails/005/337/799/small/icon-image-not-found-free-vector.jpg'></div>
       <div class="Text">
       <h4 class="Name">${mnti.mntnm}</h4>
       </div>
+      <a href="list.html?page=details&mountain_keyword="${mnti.mntnm}"></a>
       </article>
       `;
     })
@@ -110,7 +138,7 @@ const render = () => {
 const paginationRender = async() => {
   const keyword = document.getElementById("SearchName").value;
   url = new URL(
-    `http://openapi.forest.go.kr/openapi/service/cultureInfoService/gdTrailInfoOpenAPI?ServiceKey=${MOUNTAINS_KEY}&numOfRows=${numOfRows}&searchMtNm=${keyword}&_type=json`
+    `${TRAIL_URL}openapi/service/cultureInfoService/gdTrailInfoOpenAPI?ServiceKey=${MOUNTAINS_KEY}&numOfRows=${numOfRows}&searchMtNm=${keyword}&_type=json`
   );
   url.searchParams.set("pageNo", pageNo)
   const response = await fetch(url);
@@ -118,49 +146,61 @@ const paginationRender = async() => {
 
   totalResult = data.response.body.totalCount;
 
-  const totalPages = Math.ceil(totalResult/numOfRows)
-  const pageGroup = Math.ceil(pageNo / groupSize); //현재 내가 보고 있는 페이지가 속해있는 그룹
-  console.log(totalPages)
+  let totalPages = Math.ceil(totalResult/numOfRows)
+  const pageGroup = Math.ceil(pageNo / groupSize);
   let lastPage = pageGroup * groupSize;
-  //마지막 페이지그룹이 그룹사이즈보다 작으면? lastPage = totalPage
-  if(lastPage > totalPages) {
+  let firstPage = lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
+  let pagiNationHTML = ``;
+  if (lastPage > totalPages) {
     lastPage = totalPages;
   }
-  let firstPage = lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
-  
-
-
-  let pagiNationHTML = ``;
-  if (1 < pageNo) {
-    pagiNationHTML +=
-      `<li class="page-item" onclick="moveToPage(1)">
-        <a class="page-link"><<
-        </a>
+  if (pageNo >= 2) {
+    pagiNationHTML = `
+      <li class="page-item prev ${pageNo === 1 ? "disabled" : ""}">
+        <a class="page-link" href="#" pageNum="${1}"><i class="xi-backward"></i></a>
       </li>
-      <li class="page-item" onclick="moveToPage(${pageNo - 1})">
-        <a class="page-link"><
-        </a>
-      </li>`
+      <li class="page-item prev ${pageNo === 1 ? "disabled" : ""}">
+        <a class="page-link" href="#" pageNum="${pageNo - 1}"><i class="xi-play"></i></a>
+      </li>`;
   }
-  for(let i = firstPage; i <= lastPage; i++) {
-    pagiNationHTML += `<li class="page-item ${i === pageNo ? 'active' : ''}" onclick="moveToPage(${i})"><a class="page-link" href="#">${i}</a></li>`
+  for (let i = firstPage; i <= lastPage; i++) {
+    pagiNationHTML += `
+      <li class="page-item ${i === pageNo ? "active" : ""}">
+        <a class="page-link" href="#" pageNum="${i}">${i}</a>
+      </li>`;
   }
   if (pageNo < totalPages) {
-    pagiNationHTML +=
-      `<li class="page-item" onclick="moveToPage(${pageNo + 1})">
-        <a class="page-link">
-          >
-          </a>
-          </li>
-          <li class="page-item" onclick="moveToPage(${totalPages})">
-          <a class="page-link">>>
-        </a>
-      </li>`
+    pagiNationHTML += `
+      <li class="page-item next ${pageNo === totalPages ? "disabled" : ""}">
+        <a class="page-link" href="#" pageNum="${pageNo + 1}"><i class="xi-play"></i></a>
+      </li>
+      <li class="page-item next ${pageNo === totalPages ? "disabled" : ""}">
+        <a class="page-link" href="#" pageNum="${totalPages}"><i class="xi-forward"></i></a>
+      </li>`;
   }
   document.querySelector(".pagination").innerHTML = pagiNationHTML;
+  document.querySelectorAll(".page-item").forEach((item) => {
+    item.addEventListener("click", (event) => {
+      const pageNum = parseInt(event.target.getAttribute("pageNum"));
+      moveToPage(pageNum)
+    });
+  });
 }
 const moveToPage = (pageNum) => {
   pageNo = pageNum;
   getMntiList();
+  console.log(pageNum)
 }
+
+
+const errorRender = (text) => {
+  document.querySelector(".MountainGroup").innerHTML = `
+      <div class="errorMessage">
+        <p><b>"${text}"</b>은(는) 정보가 없습니다.</p>
+      </div>`;
+}
+
+
 getMntiList();
+
+document.querySelector(".ShowMountain .heading").addEventListener("click", getMntiList)
