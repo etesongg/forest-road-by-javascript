f1f5a761886071134805818544afb92dacdff9a9
etesong014 <etesong0114@gmail.com>
2024-07-21 13:22:18 +0900

    Fix: 지도 나오도록 수정

--- static/details.js
+++ static/details.js
@@ -1,4 +1,4 @@
-import config from "../apikey.js";
+import config from "../apikey.js"
 
 const MOUNTAINS_KEY = config.KEY.mountains_key;
 const MOUNTAINS_URL = config.URL.mountains_url;
@@ -9,61 +9,44 @@ const WEATHER_KEY = config.KEY.weather_key;
 const queryParams = new URLSearchParams(window.location.search);
 const mountain_keyword = queryParams.get("searchWrd");
 
-let url = new URL(
-  `${MOUNTAINS_URL}1400000/service/cultureInfoService2/mntInfoOpenAPI2?_type=json&serviceKey=${MOUNTAINS_KEY}&searchWrd=${mountain_keyword}`
-);
+let url = new URL(`${MOUNTAINS_URL}1400000/service/cultureInfoService2/mntInfoOpenAPI2?_type=json&serviceKey=${MOUNTAINS_KEY}&searchWrd=${mountain_keyword}`)
 
 // 산 정보만 뽑을 경우
-const getMntData = async () => {
-  url = new URL(
-    `${MOUNTAINS_URL}1400000/service/cultureInfoService2/mntInfoOpenAPI2?_type=json&serviceKey=${MOUNTAINS_KEY}&searchWrd=${mountain_keyword}`
-  );
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log("getMntData", data);
-  const mntilistNo = data.response.body.items.item.mntilistno; // 산 코드
-  const mntiadd = Array.isArray(data.response.body.items.item)
-    ? data.response.body.items.item[0].mntiadd
-    : data.response.body.items.item.mntiadd; // 산 주소
-  await getMntImgData(mntilistNo);
-  await getTrailData();
-  await translateToAddress(mntiadd);
-};
-getMntData();
+const getMntData = async() => {
+    url = new URL(`${MOUNTAINS_URL}1400000/service/cultureInfoService2/mntInfoOpenAPI2?_type=json&serviceKey=${MOUNTAINS_KEY}&searchWrd=${mountain_keyword}`)
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log('getMntData',data)
+    const mntilistNo = data.response.body.items.item.mntilistno; // 산 코드
+    const mntiadd = Array.isArray(data.response.body.items.item)?data.response.body.items.item[0].mntiadd: data.response.body.items.item.mntiadd // 산 주소
+    await getMntImgData(mntilistNo);
+    await getTrailData();
+    await translateToAddress(mntiadd);
+}
+getMntData()
 
 const getMntImgData = async (mntilistNo) => {
-  url = new URL(
-    `${MOUNTAINS_URL}1400000/service/cultureInfoService2/mntInfoImgOpenAPI2?_type=json&mntiListNo=${mntilistNo}&ServiceKey=${MOUNTAINS_KEY}`
-  );
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log("getMntImgData", data);
-  const MntImgFile =
-    data.response.body.items == ""
-      ? "static/img/not-mntimgfile.jpg"
-      : Array.isArray(data.response.body.items.item)
-      ? `http://www.forest.go.kr/images/data/down/mountain/${data.response.body.items.item[0].imgfilename}`
-      : `http://www.forest.go.kr/images/data/down/mountain/${data.response.body.items.item.imgfilename}`;
-  displayMntInfoFigure(MntImgFile);
-};
+    url = new URL(`${MOUNTAINS_URL}1400000/service/cultureInfoService2/mntInfoImgOpenAPI2?_type=json&mntiListNo=${mntilistNo}&ServiceKey=${MOUNTAINS_KEY}`)
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log('getMntImgData', data)
+    const MntImgFile = (data.response.body.items == "")? "static/img/not-mntimgfile.jpg": Array.isArray(data.response.body.items.item)? `http://www.forest.go.kr/images/data/down/mountain/${data.response.body.items.item[0].imgfilename}`: `http://www.forest.go.kr/images/data/down/mountain/${data.response.body.items.item.imgfilename}`;
+    displayMntInfoFigure(MntImgFile)
+}
 
 const displayMntInfoFigure = (MntImgFile) => {
-  document.querySelector(
-    ".details-mnt-information figure"
-  ).innerHTML = `<img src="${MntImgFile}" alt="">`;
-};
+    document.querySelector(".details-mnt-information figure").innerHTML = `<img src="${MntImgFile}" alt="">`;
+}
 
 const getTrailData = async () => {
-  url = new URL(
-    `${TRAIL_URL}openapi/service/cultureInfoService/gdTrailInfoOpenAPI?_type=json&searchMtNm=${mountain_keyword}&serviceKey=${MOUNTAINS_KEY}`
-  );
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log("getTrailData", data);
-  const mntncd = data.response.body.items.item.mntncd;
-  await displayMntInfoArticle(data.response.body.items.item);
-  await getMntTrail(mntncd);
-};
+    url = new URL(`${TRAIL_URL}openapi/service/cultureInfoService/gdTrailInfoOpenAPI?_type=json&searchMtNm=${mountain_keyword}&serviceKey=${MOUNTAINS_KEY}`)
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log('getTrailData',data)
+    const mntncd = data.response.body.items.item.mntncd
+    await displayMntInfoArticle(data.response.body.items.item)
+    await getMntTrail(mntncd);
+}
 // mntncd	산코드
 // mntnm 	산명
 // subnm 	산정보부제
@@ -76,11 +59,8 @@ const getTrailData = async () => {
 // tourisminf 	주변관광정보설명
 // etccourse	산정보주변관광정보기타코스설명
 const displayMntInfoArticle = (info) => {
-  const mntiDetails =
-    info.details.length >= 300
-      ? `${info.details.slice(0, 300)}...`
-      : info.details;
-  document.querySelector(".details-mnt-information article").innerHTML = `
+    const mntiDetails = info.details.length >= 300? `${info.details.slice(0,300)}...`: info.details
+    document.querySelector(".details-mnt-information article").innerHTML = `
         <h5><b>${info.mntnm}</b></h5>
         <p><span>고도 |</span> <span>${info.mntheight} m</span></p>
         <p><span>지역 |</span> <span>${info.areanm}</span></p>
@@ -97,40 +77,36 @@ const displayMntInfoArticle = (info) => {
         <br>주변관광정보설명 | ${info.tourisminf}</b>
     </div>
     </div>`;
-
-  openTrailCourse(info.etccourse);
-};
+    
+    openTrailCourse(info.etccourse);
+}
 
 const getMntTrail = async (mntncd) => {
-  url = new URL(
-    `${TRAIL_URL}openapi/service/cultureInfoService/gdTrailInfoImgOpenAPI?_type=json&searchWrd=${mntncd}&serviceKey=${MOUNTAINS_KEY}`
-  );
-  const response = await fetch(url);
-  const data = await response.json();
-  console.log("getMntTrail", data);
-  const trailArray = data.response.body.items.item;
-  displayMntTrailInfo(trailArray);
-};
+    url = new URL(`${TRAIL_URL}openapi/service/cultureInfoService/gdTrailInfoImgOpenAPI?_type=json&searchWrd=${mntncd}&serviceKey=${MOUNTAINS_KEY}`)
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log('getMntTrail',data)
+    const trailArray = data.response.body.items.item
+    displayMntTrailInfo(trailArray)
+}
 
 // 산행 content, HTML 엔티티로 인코딩
 const decodeHTMLEntities = (text) => {
-  const parser = new DOMParser();
-  const doc = parser.parseFromString(text, "text/html");
-  return doc.documentElement.textContent;
-};
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(text, "text/html");
+    return doc.documentElement.textContent;
+}
 
 // titl	산행추가제목
 // content	산행추가설명
 // image	산행이미지순번
 // 신행 아니고 그냥 관련 사진? 같은 느낌
 const displayMntTrailInfo = async (trailArray) => {
-  const MntTrailInfoHTML = trailArray
-    .map((trail) => {
-      const splitTitle = trail.titl.includes("-")
-        ? trail.titl.split("-")[1]
-        : trail.titl;
-      const decodedContent = decodeHTMLEntities(trail.content);
-      return `
+    const MntTrailInfoHTML = trailArray.map((trail) => {
+        const splitTitle = trail.titl.includes("-")?
+        trail.titl.split("-")[1]: trail.titl;
+        const decodedContent = decodeHTMLEntities(trail.content);
+        return `
         <div class="card" style="width: 18rem;">
         <img src="http://www.forest.go.kr/swf/foreston/mountain/${trail.image}" class="card-img-top" alt="...">
         <div class="card-body">
@@ -139,56 +115,49 @@ const displayMntTrailInfo = async (trailArray) => {
             ${decodedContent}
             </p>
         </div>
-        </div>`;
-    })
-    .join("");
-  document.querySelector(".details-mnt-related-images > .row").innerHTML =
-    MntTrailInfoHTML;
-};
+        </div>`
+    }).join("");
+    document.querySelector(".details-mnt-related-images > .row").innerHTML = MntTrailInfoHTML
+}
 
 const translateToAddress = async (mntAdress) => {
-  naver.maps.Service.geocode(
-    { address: mntAdress },
-    function (status, response) {
-      console.log(mntAdress);
-      if (status === naver.maps.Service.Status.ERROR) {
-        console.log("Something wrong!");
-        console.log("Status:", status);
-        console.log("Response:", response);
-        return;
-      }
-      // 성공 시의 response 처리
-      const { x, y } = response.v2.addresses[0];
-      console.log(`y : ${x} x : ${y}`);
-      return initMap(y, x);
-    }
-  );
-};
+    naver.maps.Service.geocode({ address: mntAdress }, function(status, response) {
+        console.log(mntAdress)
+        if (status === naver.maps.Service.Status.ERROR) {
+            console.log('Something wrong!');
+            console.log('Status:', status);
+            console.log('Response:', response);
+            return;
+        }
+        // 성공 시의 response 처리
+        const {x, y} = response.v2.addresses[0]
+        console.log(`y : ${x} x : ${y}`);
+        return initMap(y, x)
+    });
+}
 
 // 지도 생성
 var map = null;
 
 function initMap(x, y) {
-  var map = new naver.maps.Map("map", {
-    center: new naver.maps.LatLng(x, y),
-    zoom: 15,
-  });
-
-  var marker = new naver.maps.Marker({
-    position: new naver.maps.LatLng(x, y),
-    map: map,
-  });
+    var map = new naver.maps.Map('map', {
+        center: new naver.maps.LatLng(x, y),
+        zoom: 15
+    });
+    
+    var marker = new naver.maps.Marker({
+        position: new naver.maps.LatLng(x, y),
+        map: map
+    });
 }
 
 const openTrailCourse = (etccourse) => {
-  const trailCourseIcon = document.querySelector(".fa-location-arrow");
-  trailCourseIcon.setAttribute("data-bs-content", etccourse);
-  new bootstrap.Popover(trailCourseIcon, {
-    html: true,
-  }); // 부트스트랩 popover 초기화 해줘야 함, html 태그 인식되도록 하기
-};
-
-// 상단 버튼 효과 추가하기 (영화)
+    const trailCourseIcon = document.querySelector(".fa-location-arrow");
+    trailCourseIcon.setAttribute("data-bs-content", etccourse);
+    new bootstrap.Popover(trailCourseIcon, {
+        html: true
+    }); // 부트스트랩 popover 초기화 해줘야 함, html 태그 인식되도록 하기
+}
 
 // const callWeather = async (y, x) => {
 //     url = new URL(`${WEATHER_URL}data/2.5/weather?lat=${y}&lon=${x}&lang=kr&units=metric&appid=${WEATHER_KEY}`)
@@ -214,5 +183,5 @@ const openTrailCourse = (etccourse) => {
 //     `
 // }
 
-/////////////////////////////////////////
-// 중간 슬로건 효과 코드
+
+
